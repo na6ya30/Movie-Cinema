d4d64b489808ef43b43b3a7bfa809b98c8659572
na6ya30 <163208686+na6ya30@users.noreply.github.com>
2024-03-14 09:33:31 +1300

    hi

---
+++ movie_tickets_v1.py
@@ -0,0 +1,15 @@
+"""Movie theatre ticketing system - v1
+Welcome screen and initialise variables
+"""
+def sell_tickets():
+    print("************ FanFare Movies - ticketing system *************")
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sale = 0
+
+# main routine
+sell_tickets()
